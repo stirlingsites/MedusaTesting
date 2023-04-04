5891e52b39ffe3ce07315f589258279754f7ae7e
Stirling Sites <smsites8758@eagle.fgcu.edu>
2023-04-04 17:05:52 -0400

    Split into methods

--- src/test/java/Proj/Class1.java
+++ src/test/java/Proj/Class1.java
@@ -21,39 +21,48 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.io.IOException;
 
-public class Class1 {
+public class Class1{
     public static WebDriver driver;
+
     @Test
-    void Packing() throws InterruptedException, IOException {
+    void openURL() throws InterruptedException, IOException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\14252\\Downloads\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://127.0.0.1:8000/login?next=/");
         driver.manage().window().maximize();
-
-
+        driver.close();
         /*
         TakesScreenshot screenshot = (TakesScreenshot) driver;
         File src = screenshot.getScreenshotAs(OutputType.FILE);
         File des = new File("C:\\Users\\14252\\screenshot-selenium\\screenshot.png");
         FileHandler.copy(src, des);
         */
+    }
+    @Test
+    void correctLogin() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\14252\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://127.0.0.1:8000/login?next=/");
+        driver.manage().window().maximize();
         driver.findElement(By.name("username")).sendKeys("smsites8758@eagle.fgcu.edu");
         driver.findElement(By.name("password")).sendKeys("alimex2021");
         driver.findElement(By.xpath("/html/body/div/section/form/div/div[3]/button")).submit();
-
         Thread.sleep(2000);
-        driver.get("http://127.0.0.1:8000/view/packing");
-
-
-        driver.findElement(By.id("Sales Order")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/main/div[2]/div/form/input[2]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"dataGridContainerUp\"]/div/div[6]/div/div/div[1]/div/table/tbody/tr[1]/td[1]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"dataGridContainerUp\"]/div/div[4]/div/div/div[3]/div[1]/div/button/div/span")).click();
-        Thread.sleep(2000);
-
         driver.close();
     }
+    @Test
+    void invalidLogin() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\14252\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://127.0.0.1:8000/login?next=/");
+        driver.manage().window().maximize();
+        driver.findElement(By.name("username")).sendKeys("bademail@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.name("password")).sendKeys("alimex");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div/section/form/div/div[3]/button")).submit();
+        Thread.sleep(4000);
+        driver.close();
+    }
+
 }
--- src/test/java/Proj/Packing.java
+++ src/test/java/Proj/Packing.java
@@ -3,20 +3,31 @@ package Proj;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class Packing {
         public static WebDriver driver;
-
-        public static void main(String[] args) throws InterruptedException, IOException {
+        @Test
+        void Packing() throws InterruptedException {
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\14252\\Downloads\\chromedriver_win32\\chromedriver.exe");
             driver = new ChromeDriver();
-            driver.get("http://127.0.0.1:8000/view/packing");
+            driver.get("http://127.0.0.1:8000/login?next=/");
             driver.manage().window().maximize();
+            driver.findElement(By.name("username")).sendKeys("smsites8758@eagle.fgcu.edu");
+            driver.findElement(By.name("password")).sendKeys("alimex2021");
+            driver.findElement(By.xpath("/html/body/div/section/form/div/div[3]/button")).submit();
+            driver.get("http://127.0.0.1:8000/view/packing");
             driver.findElement(By.id("Sales Order")).click();
-            Thread.sleep(200);
-            driver.findElement(By.name("Logon")).click();
             Thread.sleep(2000);
+            driver.findElement(By.xpath("/html/body/main/div[2]/div/form/input[2]")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//*[@id=\"dataGridContainerUp\"]/div/div[6]/div/div/div[1]/div/table/tbody/tr[1]/td[1]")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//*[@id=\"dataGridContainerUp\"]/div/div[4]/div/div/div[3]/div[1]/div/button/div/span")).click();
+            Thread.sleep(2000);
+
+            driver.close();
         }
 }
